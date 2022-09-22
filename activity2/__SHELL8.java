707af4c37f2ffba7c8617e76da98e583cbea7aef
Oscar DeHamer <oscar.dehamer@mrjaffesclass.com>
2022-09-22 12:21:39 -0700

    finshied activity 3

---
+++ activity2/__SHELL8.java
@@ -0,0 +1,8 @@
+package activity2;
+public class __SHELL8 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+
+java.lang.String[] __bluej_param0 = { };
+activity2.CopyOfMagpieRunner2.main(__bluej_param0);
+
+}}
